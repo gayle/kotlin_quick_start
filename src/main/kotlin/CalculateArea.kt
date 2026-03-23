7e21109fed43ee122b1d4cd29b12ba6f261bb4cb
gayle <gayle_999@yahoo.com>
2026-03-23 17:29:32 -0400

    calculate circle area

---
+++ src/main/kotlin/CalculateArea.kt
@@ -0,0 +1,11 @@
+import kotlin.math.pow
+
+// Write a program that generates a random circle radius between 1 and 10
+// and calculates the circle's area.
+fun main(args : Array<String>) {
+    val radius = Math.random() * 10
+    println("radius = $radius")
+    // val area = Math.PI * (radius * radius)
+    val area = Math.PI * (radius.toDouble().pow(2))
+    println("area = $area")
+}
