69cade48edcf5a79a721a3bb63f445375c5fe528
gayle <gayle_999@yahoo.com>
2026-04-10 16:07:34 -0400

    Add instruction text to file.

--- src/main/kotlin/FunctionArraysOrdered.kt
+++ src/main/kotlin/FunctionArraysOrdered.kt
@@ -1,3 +1,6 @@
+// Function Declaration and Usage
+// Write a function that accepts an ary of int, and returns true if the array is ordered.
+// Accpet a second default param that determines if they array is sorted in asc or desc order.
 fun main (args: Array<String>) {
     var arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     println(is_ordered(arr))
